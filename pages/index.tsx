5bf40964baa08ad67df14f6110b9548ad288a3d3
Devex35 <exanton904@gmail.com>
2022-06-01 04:14:46 +0100

    start

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Body from '../components/Body'
+import Contact from "../components/Contact"
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Hero />
+      <Body />
+      <Contact/>
+    </div>
+  )
+}
+
+export default Home
