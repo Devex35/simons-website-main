1900fc8ae27422634db682a0b5715fe9fbd3a642
Devex35 <exanton904@gmail.com>
2022-06-14 18:59:10 +0100

    Image gallary

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import Navbar from './Navbar'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      {children}
+    </div>
+  )
+}
+
+export default Layout
